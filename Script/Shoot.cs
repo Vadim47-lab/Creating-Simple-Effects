2d6b0096d4ff622d117e4eb7319131af0b898cb8
Stepanov Vadim <vadgat777@mail.ru>
2022-05-16 15:18:44 +0700

    14 commit

---
+++ Script/Shoot.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    [SerializeField] private Camera _shootingCamera;
+    [SerializeField] private GameObject _effect;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = _shootingCamera.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1));
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100f))
+            {
+                var effect = Instantiate(_effect, hit.point, Quaternion.identity);
+                effect.transform.rotation = Quaternion.FromToRotation(effect.transform.forward, hit.normal) * effect.transform.rotation;
+            }
+        }
+    }
+}
